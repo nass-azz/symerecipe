fd29820229f96fed3ebf838a3184dd75bdc2bcca
unknown <nassimazzouzi59@gmail.com>
2026-06-02 20:26:55 +0200

    Partie 3 : CRUD des recettes - Créer une recette

--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -11,22 +11,46 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class RecipeController extends AbstractController
 {
-    #[Route('/recipe', name: 'app_recipe', methods: ['GET'])]
+    #[Route('/recipe', name: 'app_recipe', methods: ['GET', 'POST'])]
     public function index(
         RecipeRepository $repository,
         PaginatorInterface $paginator,
         Request $request
     ): Response {
-        // On récupère toutes les recettes et on les pagine (10 par page)
         $recipes = $paginator->paginate(
             $repository->findAll(),
             $request->query->getInt('page', 1),
             10
         );
 
-        // On envoie le tout à la vue Twig
         return $this->render('pages/recipe/index.html.twig', [
             'recipes' => $recipes,
         ]);
     }
-}
+
+    #[Route('/recipe/new', name: 'recipe_new', methods: ['GET', 'POST'])]
+    public function new(Request $request,
+        \Doctrine\ORM\EntityManagerInterface $manager
+    ):Response {
+        $recipe = new \App\Entity\Recipe();
+
+        $form = $this->createForm(\App\Form\RecipeType::class, $recipe);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $recipe = $form->getData();
+
+            $manager->persist($recipe);
+            $manager->flush();
+
+            $this->addFlash('success', 'Votre recette a été ajoutée avec succès !');
+            return $this->redirectToRoute('app_recipe');
+        }
+
+        return $this->render('pages/recipe/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+}   
